6e410b5c165a23b25c6fc06eaa115e873ce6909a
Dmitriy D <akenory@gmail.com>
2025-11-03 13:08:26 +0500

    Initial commit

---
+++ SwiftUIBase/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  SwiftUIBase
+//
+//  Created by Дмитрий Дудник on 03.11.2025.
+//
+
+import SwiftUI
+
+struct Post: Identifiable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let image: Image
+}

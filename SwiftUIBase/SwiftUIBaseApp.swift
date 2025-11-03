e09e6abf1602ea66e962c62c6ffa5495520354bf
Dmitriy D <akenory@gmail.com>
2025-11-03 12:57:04 +0500

    Initial Commit

---
+++ SwiftUIBase/SwiftUIBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBaseApp.swift
+//  SwiftUIBase
+//
+//  Created by Дмитрий Дудник on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
